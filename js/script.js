86eb5b655edfb9f894bb5a22404deb4ee0285417
thales15 <166405797+thales15@users.noreply.github.com>
2025-05-25 16:53:18 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,51 @@
+
+function FazerPedido(event) {
+    event.preventDefault();
+
+    let nome = document.getElementById('nome').value;
+    let endereco = document.getElementById('endereco').value;
+    let pizza = document.getElementById('pizza').value;
+    let OBS = document.getElementById('observacoes').value;
+
+    sessionStorage.setItem('username', nome);
+    sessionStorage.setItem('endereco', endereco);
+    sessionStorage.setItem('pizza', pizza);
+    sessionStorage.setItem('obs', OBS);
+
+
+    Swal.fire({
+        title: "Pedido enviado!!",
+        text: "Pedido realizado com sucesso!",
+        icon: "success"
+        , buttonsStyling: 'false'
+    });
+}
+function FazerCadastro(event) {
+    event.preventDefault();
+
+    let nome = document.getElementById('nome').value;
+    let email = document.getElementById('email').value;
+    let telefone = document.getElementById('telefone').value;
+    let senha = document.getElementById('senha').value;
+    let confirmar = document.getElementById('confirmarSenha').value;
+
+    if (senha == confirmar) {
+        localStorage.setItem('nome', nome)
+        localStorage.setItem('email', email)
+        localStorage.setItem('telefone', telefone)
+        localStorage.setItem('senha', senha)
+
+        Swal.fire({
+            title: "Cadastrado!!",
+            text: "Cadastro realizado com êxito",
+            icon: "success"
+            , buttonsStyling: 'false'
+        });
+
+
+    } else {
+        alert('confirmar senha está errado')
+    }
+}
+
+
